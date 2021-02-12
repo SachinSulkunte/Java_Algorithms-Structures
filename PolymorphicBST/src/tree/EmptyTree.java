30b4929bce19bd0dc11f29cbc0d1082ede731504
SachinSulkunte <41236722+SachinSulkunte@users.noreply.github.com>
2021-02-11 19:11:57 -0500

    Polymorphic Binary Search Trees - Creation, Manipulation

--- PolymorphicBST/src/tree/EmptyTree.java
+++ PolymorphicBST/src/tree/EmptyTree.java
@@ -2,24 +2,9 @@ package tree;
 
 import java.util.Collection;
 
-/**
- * This class is used to represent the empty search tree: a search tree that
- * contains no entries.
- * 
- * This class is a singleton class: since all empty search trees are the same,
- * there is no need for multiple instances of this class. Instead, a single
- * instance of the class is created and made available through the static field
- * SINGLETON.
- * 
- * The constructor is private, preventing other code from mistakenly creating
- * additional instances of the class.
- *  
- */
  public class EmptyTree<K extends Comparable<K>,V> implements Tree<K,V> {
 	/**
 	 * This static field references the one and only instance of this class.
-	 * We won't declare generic types for this one, so the same singleton
-	 * can be used for any kind of EmptyTree.
 	 */
 	private static EmptyTree SINGLETON = new EmptyTree();
 
@@ -33,7 +18,7 @@ import java.util.Collection;
 	 *  
 	 */
 	private EmptyTree() {
-		// Nothing to do
+		
 	}
 	
 	public V search(K key) { //node is empty
